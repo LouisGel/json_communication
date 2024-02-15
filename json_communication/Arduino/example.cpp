e2f5f1349b21e1cadd1d81488827e1677cd742da
LouisGel <58270781+LouisGel@users.noreply.github.com>
2024-02-15 14:41:23 -0500

    Add files via upload

---
+++ json_communication/Arduino/example.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include "communication.hpp"
+
+void onMessage(StaticJsonDocument<500>& doc);
+
+Commuication<500> com;                          //On crée une communication
+
+void setup() {
+  Serial.begin(9600);                           //On set normalement notre Serial
+  com.setSerial(&Serial);                       //Ici on passe par quel Serial la communication sera faite
+  com.onMessageFunc(&onMessage);                //On indique quoi faire quand on reçois un message
+}
+
+void loop() {
+    com.update();                               //Dans la loop principale, on update la communication
+}
+
+void onMessage(StaticJsonDocument<500>& doc)    //! La variable "doc" contient le message reçus
+{
+    com.sendMessage(doc);                       //? Ici on renvois le message reçus tel quel
+}
